ea1376fd68ded3a17e98d673a5bd83e62f8558b2
victor ratts <victor.ratts13@gmail.com>
2021-08-27 18:09:42 -0300

    new version

---
+++ core.js
@@ -0,0 +1,10 @@
+// core version 1.0 - bettsttar365
+
+/*
+    init code with jquery and socket.io.
+    create modules for connection
+    and build a structure for all sintaxy
+
+*/
+
+var ws = io('http://localhost:8001');
